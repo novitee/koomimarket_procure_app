2acb950de3ff6ee7b8257c555b04d4bc2dbd828c
GlifeDylan <dylan@glife.com.sg>
2023-09-14 19:17:43 +0700

    remove unused code

--- src/utils/transition.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { TransitionPresets } from '@react-navigation/stack';
-
-export const TransitionScreenOptions = () => {
-  return {
-    ...TransitionPresets.SlideFromRightIOS
-  }
-}
